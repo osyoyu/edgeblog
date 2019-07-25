b6d288816fdcf91d1686f878f29612c5d8bc8396
osyoyu <osyoyu@osyoyu.com>
2019-07-26 01:53:56 +0900

    Add sample Markdown conversion

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
 use http_guest::{Request, Response, guest_app};
+use comrak::{markdown_to_html, ComrakOptions};
 
 pub fn user_entrypoint(_req: &Request<Vec<u8>>) -> Response<Vec<u8>> {
+    let markdown = r#"
+Sample Markdown
+===
+
+This is a sample Markdown document, and here's a link to [Google](https://google.com).
+"#;
+
+    let html = markdown_to_html(markdown, &ComrakOptions::default());
     Response::builder()
         .status(200)
-        .body("Hello, world!".as_bytes().to_owned())
+        .header("content-type", "text/html; charset=utf-8")
+        .body(html.as_bytes().to_owned())
         .unwrap()
 }
 
